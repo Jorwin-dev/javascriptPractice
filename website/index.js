7950c3b2e1120f871f68ef21213879459fbbec31
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2024-01-04 14:25:38 -0600

    Basics of Variables in JavaScript.

--- website/index.js
+++ website/index.js
@@ -1,10 +1,28 @@
-    /*
+/* JavaScript Basics:
+    // Print to console.
     console.log(`Hello`);
     console.log(`I like pizza!`);
-    // Backticks `` are to create a template literal
+    // Backticks `` are to create a template literal.
     window.alert(`This is an alert!`);
     window.alert(`I like pizza`);
-   // Use to change text content of an HTML element
+   // Use to change text content of an HTML element.
    document.getElementById("myH1").textContent = `Hello`;
    document.getElementById("myP").textContent = `I like pizza!`;
-   */
+*/
+
+/* Variables:
+   // declaration
+   let x;
+
+   // assignment
+   x = 100;
+
+   // declaration + assignment
+   let age = 21;
+
+   // return type at console
+    console.log(typeof age);
+
+   // print/return to console
+   console.log(`You are ${age} years old.`);
+*/
