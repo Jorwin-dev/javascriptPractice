f73bc886f11375ca14eb602a04b47c744dcc173b
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2024-01-04 14:08:52 -0600

    Add basics of JavaScript.

--- website/index.js
+++ website/index.js
@@ -1,1 +1,10 @@
-    
+    /*
+    console.log(`Hello`);
+    console.log(`I like pizza!`);
+    // Backticks `` are to create a template literal
+    window.alert(`This is an alert!`);
+    window.alert(`I like pizza`);
+   // Use to change text content of an HTML element
+   document.getElementById("myH1").textContent = `Hello`;
+   document.getElementById("myP").textContent = `I like pizza!`;
+   */
