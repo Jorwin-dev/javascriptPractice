fce96eb6aab90797ab767ec6a6a9971619e0591f
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2024-01-06 11:14:27 -0600

    Accepting User Input.

--- website/index.js
+++ website/index.js
@@ -1,16 +1,16 @@
-/* JavaScript Basics:
-    // Print to console.
-    console.log(`Hello`);
-    console.log(`I like pizza!`);
-    // Backticks `` are to create a template literal.
-    window.alert(`This is an alert!`);
-    window.alert(`I like pizza`);
+/* 1) JavaScript Basics:
+   // Print to console.
+   console.log(`Hello`);
+   console.log(`I like pizza!`);
+   // Backticks `` are to create a template literal.
+   window.alert(`This is an alert!`);
+   window.alert(`I like pizza`);
    // Use to change text content of an HTML element.
    document.getElementById("myH1").textContent = `Hello`;
    document.getElementById("myP").textContent = `I like pizza!`;
 */
 
-/* Variables:
+/* 2) Variables:
    // declaration
    let x;
 
@@ -25,4 +25,25 @@
 
    // print/return to console
    console.log(`You are ${age} years old.`);
+*/
+
+/* 3) Accepting user input:
+   // Window Prompt
+   let username;
+   
+   username = window.prompt("What's your username?");
+
+   console.log(username);
+
+   // HTML text-box
+   <h1 id="myH1"></h1>
+   <label>username: </label>
+   <input id="myText"><br><br>
+   <button id="mySubmit">submit</button>
+
+   let username;
+   document.getElementById("mySubmit").onClick = function(){
+      username = document.getElementById("myText").value;
+      document.getElementById("myH1").textContent = `Hello ${username}`;
+   }
 */
